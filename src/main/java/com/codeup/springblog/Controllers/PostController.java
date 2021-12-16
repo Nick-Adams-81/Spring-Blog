cffb8ee7b982dccaab6fbf8577c247a3dfb5fb7b
nick adams <thebigdeal916@icloud.com>
2021-12-16 14:57:20 -0600

    added third spring exercise

---
+++ src/main/java/com/codeup/springblog/Controllers/PostController.java
@@ -0,0 +1,35 @@
+package com.codeup.springblog.Controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String postsIndex() {
+        return "all the posts here soon";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String postById() {
+        return "Getting posts by id herre";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String viewPosts() {
+        return "Viewing posts here";
+    }
+
+    @PostMapping("posts/create")
+    @ResponseBody
+    public String createNewPost() {
+        return "New posts go through here";
+    }
+}
