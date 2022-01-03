7144e85c5ece25f1cbe51a0bcf1652d0d0635b7a
Nick Adams <nick.pappas.adams@gmail.com>
2022-01-03 14:04:22 -0600

    added navbar and dice roll exercises

--- src/main/java/com/codeup/springblog/Controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/Controllers/HomeController.java
@@ -29,4 +29,40 @@ public class HomeController {
         model.addAttribute("cohort", "Welcome to " + cohort + "!");
         return "join";
     }
+
+    @GetMapping("roll-dice")
+    public String RollDice() {
+        return "roll";
+    }
+
+    @GetMapping("roll-dice/1")
+    public String guess1() {
+        return "guess1";
+    }
+
+    @GetMapping("roll-dice/2")
+    public String guess2() {
+        return "guess2";
+    }
+
+    @GetMapping("roll-dice/3")
+    public String guess3() {
+        return "guess3";
+    }
+
+    @GetMapping("roll-dice/4")
+    public String guess4() {
+        return "guess4";
+    }
+
+    @GetMapping("roll-dice/5")
+    public String guess5() {
+        return "guess5";
+    }
+
+    @GetMapping("roll-dice/6")
+    public String guess6() {
+        return "guess6";
+    }
+
 }
