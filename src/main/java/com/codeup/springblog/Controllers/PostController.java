42eb4c6ee0b6e3a95f7893b4797e4b8659c6e513
Nick Adams <nick.pappas.adams@gmail.com>
2022-01-06 16:12:42 -0600

    added username to view of posts

--- src/main/java/com/codeup/springblog/Controllers/PostController.java
+++ src/main/java/com/codeup/springblog/Controllers/PostController.java
@@ -34,15 +34,12 @@ public class PostController {
     @GetMapping("posts/edit/{id}")
     public String editPost(@PathVariable long id, Model model) {
         Post editPost = postDao.getById(id);
-        System.out.println("editPost.getTitle() = " + editPost.getTitle());
-        System.out.println("editPost.getBody() = " + editPost.getBody());
         model.addAttribute("postToEdit", editPost);
         return "posts/edit";
     }
 
     @PostMapping("/posts/edit/{id}")
     public String saveEditedPost(@PathVariable long id, @RequestParam(name="postTitle") String postTitle, @RequestParam(name="postBody") String postBody) {
-
         Post postToEdit = postDao.getById(id);
         postToEdit.setBody(postBody);
         postToEdit.setTitle(postTitle);
